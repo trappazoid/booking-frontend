f94d184d3147e643220c9b73c6b5590000de51a5
trappazoid <topaztelega@gmail.com>
2026-02-07 12:35:59 +0100

    Update api.js
    ?

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://booking-backend-93ye.onrender.com';
+const API_BASE_URL = 'https://booking-backend-93ye.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
